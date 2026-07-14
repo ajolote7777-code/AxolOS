d4e44d95b8a0472aa0767991ec988ece639e133f
ajolote7777-code <ajoloteruur@gmail.com>
2026-07-14 09:26:58 +0200

    working clock and topbar, missing tpo choose a colour for this top bar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function update() {
     const now = new Date();
     const hour =  now.toLocaleTimeString("es-ES");
-    document.getElementById("clock").textContent = hora;
+    document.getElementById("clock").textContent = hour;
 }
 update();
 setInterval(update, 1000);

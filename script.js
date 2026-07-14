ce562bbc20ebdd8e74e2a8667ec0e9a78ce4002d
ajolote7777-code <ajoloteruur@gmail.com>
2026-07-14 20:42:19 +0200

    tried adding a movable window in cajaprincipal but failed. Now Spain is gonna play in the WC, I'll try to fix it tomorrow morning!

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ function update() {
 }
 update();
 setInterval(update, 1000);
+function DragElement(cajaprincipal){
+
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+
+    if(document.getElementById(cajaprincipal.id + "Header")){
+        document.getElementById(cajaprincipal.id + "Header").onmousedown = startDragging;
+
+    }
+    else {
+        cajaprincipal.onmousedown = startDragging;
+
+    }
+    function startDragging(e){
+        e = e || window.Event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX
+        initialY = e.clientY
+        Element.style.top = (Element.offstetTop - currentY) + "px";
+        Element.style.left =(Element.offsetLeft - currentX) + "px";
+
+    }
+    function stopDragging(){
+        document.onmouseup = null;
+        document.onmousemove= null;
+    }
+
+    }
